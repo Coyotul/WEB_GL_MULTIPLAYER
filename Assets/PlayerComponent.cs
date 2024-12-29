e025bfc2b659631fa35d9332794f653ebf59f965
Constantin Florin-George <constantingeorge150@gmail.com>
2024-12-29 22:31:48 +0200

    adding skyboxes, adding server on repo and finish

--- Assets/PlayerComponent.cs
+++ Assets/PlayerComponent.cs
@@ -1,22 +1,29 @@
+using System;
 using UnityEngine;
 using NativeWebSocket;
+using TMPro;
 
 public class PlayerComponent : MonoBehaviour
 {
     private Vector3 _currentPosition;
     private Camera _camera;
     
-    private float speed = 5f;
-    private float cameraSpeed = 5f;
+    private float _speed = 5f;
+    private float _cameraSpeed = 5f;
+    private bool _isGrounded;
 
     private WebSocket _socket;
+    private GameObject _finishText;
 
-    [SerializeField] private string playerName;
+    [SerializeField] private string _playerName;
 
     private void Start()
     {
-        Set3DNametag(playerName);
+        Set3DNametag(_playerName);
         _camera = Camera.main;
+        
+        var finishText = GameObject.FindGameObjectWithTag("Finish");
+        _finishText = finishText;
     }
 
     private void Update()
@@ -27,13 +34,13 @@ public class PlayerComponent : MonoBehaviour
     
     public string GetPlayerName()
     {
-        return playerName;
+        return _playerName;
     }
     
     private void MoveCamera()
     {
         Vector3 cameraPosition = new Vector3(_currentPosition.x, _currentPosition.y, -10);
-        _camera.transform.position = Vector3.Lerp(_camera.transform.position, cameraPosition, cameraSpeed * Time.deltaTime);
+        _camera.transform.position = Vector3.Lerp(_camera.transform.position, cameraPosition, _cameraSpeed * Time.deltaTime);
     }
     
     public Position GetPosition()
@@ -51,23 +58,44 @@ public class PlayerComponent : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizontal, vertical, 0) * speed * Time.deltaTime;
+        Vector3 movement = new Vector3(horizontal, vertical, 0) * _speed * Time.deltaTime;
         _currentPosition = transform.position + movement;
 
         transform.Translate(movement, Space.World);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.Impulse);
+        }
     }
     
     private void Set3DNametag(string name)
     {
         GameObject nametag = new GameObject("Nametag");
         nametag.transform.SetParent(transform);
-        nametag.transform.localPosition = new Vector3(0, 0.2f, 0);
+        nametag.transform.localPosition = new Vector3(0, 0.4f, 0);
         TextMesh textMesh = nametag.AddComponent<TextMesh>();
         textMesh.text = name;
         textMesh.characterSize = 0.1f;
-        textMesh.fontSize = 20;
+        textMesh.fontSize = 30;
         textMesh.anchor = TextAnchor.MiddleCenter;
         textMesh.alignment = TextAlignment.Center;
         textMesh.color = Color.black;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Finish")
+        {
+            _finishText.GetComponent<TextMeshProUGUI>().text = "You won!";
+        }
+        else
+        {
+            _isGrounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision other)
+    {
+        _isGrounded = false;
     }
 }
--- Assets/ServerPlayerComponent.cs
+++ Assets/ServerPlayerComponent.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using NativeWebSocket;
 using System.Collections.Generic;
 
@@ -8,5 +9,14 @@ public class ServerPlayerComponent : MonoBehaviour
     public void SetPosition(Position position)
     {
         transform.position = new Vector3(position.x, position.y, position.z);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Finish")
+        {
+            var finishText = GameObject.Find("FinishText");
+            finishText.GetComponent<TextMesh>().text = "Game Over!";
+        }
     }
 }
